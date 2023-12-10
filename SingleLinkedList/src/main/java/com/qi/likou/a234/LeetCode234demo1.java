dae21e79092d12e6bd6f025e20b037f716b0d246
wuqi <375183614@qq.com>
2023-12-10 16:51:35 +0800

    LeetCode876

---
+++ SingleLinkedList/src/main/java/com/qi/likou/a234/LeetCode234demo1.java
@@ -0,0 +1,26 @@
+package com.qi.likou.a234;
+
+import com.qi.ListNode;
+
+/**
+ * 判断回文链表
+ * <br/>
+ * 输入：head=[1,2,2,1]
+ * 输出：true
+ * <br/>
+ * 解题思路：找出链表中间点，把中间点右边的链表反转，逐一比较反转后的链表与中间点左边的链表是否一致。
+ */
+public class LeetCode234demo1 {
+    /*
+    步骤1，找中间点
+    步骤2：中间点后半个链表反转
+    步骤3：反转后链表与原链表逐一比较
+     */
+//    public ListNode isPalindrome(ListNode head) {
+//
+//    }
+//    //找中间点
+//    private ListNode middle(ListNode head) {
+//
+//    }
+}
