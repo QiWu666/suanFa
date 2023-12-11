a9b0e1f765ab8ac8fe7aafb3753d0227d30e89fc
wuqi <375183614@qq.com>
2023-12-11 17:09:25 +0800

    LeetCode234

--- SingleLinkedList/src/main/java/com/qi/likou/a234/LeetCode234demo1.java
+++ SingleLinkedList/src/main/java/com/qi/likou/a234/LeetCode234demo1.java
@@ -16,11 +16,44 @@ public class LeetCode234demo1 {
     步骤2：中间点后半个链表反转
     步骤3：反转后链表与原链表逐一比较
      */
-//    public ListNode isPalindrome(ListNode head) {
-//
-//    }
-//    //找中间点
-//    private ListNode middle(ListNode head) {
-//
-//    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode middle = middle(head);
+        ListNode newList = reverse(middle);
+        while (newList != null) {
+            if (newList.val!= head.val) {
+                return false;
+            }
+            newList = newList.next;
+            head = head.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode o1) {
+        ListNode n1 = null;
+        while (o1 != null) {
+            ListNode o2 = o1.next;
+            o1.next = n1;
+            n1 = o1;
+            o1 = o2;
+        }
+        return n1;
+    }
+    //找中间点
+    private ListNode middle(ListNode head) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+        while (p2 != null && p2.next != null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        return p1;
+    }
+
+    public static void main(String[] args) {
+        ListNode nodes = ListNode.addNodes(1, 2, 2, 1,2);
+        ListNode nodes2 = ListNode.addNodes(1, 2, 3, 2, 1);
+        System.out.println(new LeetCode234demo1().isPalindrome(nodes));
+        new LeetCode234demo1().isPalindrome(nodes2);
+    }
 }
